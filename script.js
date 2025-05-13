af5fe6ff2a8726179aea7c263f2d6621d5b32490
Shriya-Salian <salianshriya21@gmail.com>
2025-05-13 13:37:57 +0530

    both arrow function

--- script.js
+++ script.js
@@ -2,21 +2,33 @@ const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 const slider = document.querySelector('.slider');
 const images = document.querySelectorAll('.image');
-
+const length = images.length;
 
 let slideNumber = 1;
 
-
-right.addEventListener('click',()=>{
-    // console.log('its working');
-
-    if(slideNumber < images.length){
-         slider.style.transform =`translateX(-${slideNumber*500}px)`; 
+const nextSlide = () =>{
+    slider.style.transform =`translateX(-${slideNumber*500}px)`; 
     slideNumber++;
-    }
-   else{
+};
+
+const prevSlide = () =>{
+    slider.style.transform =`translateX(-${(slideNumber-2)*500}px)`; 
+    slideNumber--;
+};
+
+const getFirstSlide = ()=>{
      slider.style.transform =`translateX(0px)`; 
     slideNumber =1;
-   }
-    // console.log("does this even work??");
+}
+
+const getLastSlide = ()=>{
+     slider.style.transform =`translateX(-${(length-1)*500}px)`; 
+    slideNumber =length;
+}
+right.addEventListener('click',()=>{ 
+    slideNumber < length ? nextSlide() : getFirstSlide();  
+});
+
+left.addEventListener('click',()=>{ 
+    slideNumber > 1 ? prevSlide() : getLastSlide();  
 });
